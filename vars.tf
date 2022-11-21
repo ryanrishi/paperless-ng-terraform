525a9d7179a240433206f94a62eaf7c57f882cad
Ryan Rishi <ryan@ryanrishi.com>
2022-11-20 18:03:45 -0800

    specify AZ to avoid cycle :(

--- vars.tf
+++ vars.tf
@@ -13,3 +13,12 @@ variable "paperless_admin_password" {
   default     = null
   sensitive   = true
 }
+
+variable "availability_zone" {
+  # I don't really want to specify an AZ, but since `aws_ebs_volume` requires an AZ, we can't use `aws_instance.web.availability_zone` because the EC2 instance formats/mounts the EBS volume.
+  # Alternatives considered: sleep in cloud-init (ew), run `terraform apply` + `tf taint aws_instance.web` + `tf apply`
+  # Really want to keep cloud-init as the source of truth for what's going on in an instance
+  #
+  # As an unintentional benefit, removing the aws_instance dependency from aws_ebs_volume means that recreating the instance won't wipe out the volume.
+  default = "us-west-2a"
+}
